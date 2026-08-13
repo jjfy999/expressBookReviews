68e6df3555f9e213fb6e59b6893cc356727067f9
jjfy999 <justinyehjiafeng@gmail.com>
2026-08-13 03:26:44 -0400

    update: register code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -8,7 +8,18 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!isValid(username)) {
+        users.push({'username': username, 'password': password});
+        return res.status(200).json({message: 'Login Successful!'});
+    } else {
+        return res.status(404).json({message: 'Unable with same username already exists!'});
+    }
+  }
+  return res.status(404).json({message: 'Unable to register User'});
 });
 
 // Get the book list available in the shop
